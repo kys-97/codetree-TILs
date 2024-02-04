8f0b84579c19a87a36294ba33303d9f1facf5087
kys-97 <115993353+kys-97@users.noreply.github.com>
2024-02-04 18:24:31 +0900

    Title: 두 실수의 곱, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240204/두 실수의 곱/the-product-of-two-real-numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        double a = 5.26;
+        double b = 8.27;
+        double result = a*b;
+        double temp;
+        temp = Math.round(result * 1000)/1000.0;
+        System.out.println(temp);
+
+    }
+}
