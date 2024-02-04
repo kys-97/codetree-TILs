065d0a5ed07fc563b8b5f4851f28b2d5f8665482
kys-97 <115993353+kys-97@users.noreply.github.com>
2024-02-04 19:33:25 +0900

    Title: 전화번호 바꾸기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240204/전화번호 바꾸기/change-phone-number.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("-");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println("0"+a+"-"+c+"-"+b);
+    }
+}
