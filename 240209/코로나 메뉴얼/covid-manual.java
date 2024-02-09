0b5cd44881b6808fc1363967ed8768c2ddb8bee6
kys-97 <115993353+kys-97@users.noreply.github.com>
2024-02-09 23:21:32 +0900

    Title: 코로나 메뉴얼, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240209/코로나 메뉴얼/covid-manual.java
+++ 240209/코로나 메뉴얼/covid-manual.java
@@ -4,23 +4,30 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
-        int b = sc.nextInt();
+        int a1 = sc.nextInt();
 
-        if (a.equals("Y")) {
-            if (b >= 37) {
-                System.out.println("A");
+        String b = sc.next();
+        int b1 = sc.nextInt();
+
+        String c = sc.next();
+        int c1 = sc.nextInt();
+
+        if ((a.equals("Y"))||(b.equals("Y"))||(b.equals("Y"))) {
+            if ((a1>=37)&&(b1>=37)) {
+                System.out.println("E");
             }
-            else {
-                System.out.println("C");
+            if (((b1>=37)&&(c1>=37))) {
+                System.out.println("E");
             }
-        }
-        else if (a.equals("N")) {
-            if (b >= 37) {
-                System.out.println("B");
+            if (((a1>=37)&&(c1>=37))) {
+                System.out.println("E");
+            }
+            if (((a1 >= 37)&&(b1>=37)&&(c1>=37))) {
+                System.out.println("E");
             }
         }
         else {
-            System.out.println("D");
+            System.out.println("N");
         }
     }
 }
