e99721d45edd96408feba5d287606fd71239d00c
kys-97 <115993353+kys-97@users.noreply.github.com>
2024-02-09 21:20:32 +0900

    Title: 시력 검사, Time: 157ms, Memory: 11MB, Status: Passed - CodeTree

--- 240209/시력 검사/eye-test.java
+++ 240209/시력 검사/eye-test.java
@@ -9,7 +9,7 @@ public class Main {
         if ((a >= 1.0) && (b >= 1.0)) {
             System.out.println("High");
         }
-        else if ((a>= 0.5) && (a >= b)) {
+        else if ((a>= 0.5) && (b >= 0.5)) {
             System.out.println("Middle");
         }
         else {
