1c797b3d2530a88930a33163d84b32392f95282e
kys-97 <115993353+kys-97@users.noreply.github.com>
2024-02-09 23:06:16 +0900

    Title: 윤년인가, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

--- 240209/윤년인가/is-leap-year.java
+++ 240209/윤년인가/is-leap-year.java
@@ -4,12 +4,8 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int y = sc.nextInt();
-
-        if(y%4==0 && y%100 !=0) {
+        if ((y%4 ==0 && y%100 != 0) || y%400 == 0) {
             System.out.println("true");
-            if ((y%100==0)&&(y%400==0)) {
-                System.out.println("true");
-            }
         }
         else {
             System.out.println("false");
