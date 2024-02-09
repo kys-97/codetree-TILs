9a97868ec4e2ed943d48c4106e879fb80984c2f6
kys-97 <115993353+kys-97@users.noreply.github.com>
2024-02-09 19:45:53 +0900

    Title: 알파벳에 따른 평가, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240209/알파벳에 따른 평가/evaluation-by-alphabet.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc  = new Scanner(System.in);
+        String str = sc.next();
+        if (str.equals("S")) {
+            System.out.println("Superior");
+        }
+        else if (str.equals("A")) {
+            System.out.println("Excellent");
+        }
+        else if (str.equals("B")) {
+            System.out.println("Good");
+        }
+        else if (str.equals("C")) {
+            System.out.println("Usually");
+        }
+        else if (str.equals("D")) {
+            System.out.println("Effort");
+        }
+        else {
+            System.out.println("Failure");
+        }
+    }
+}
