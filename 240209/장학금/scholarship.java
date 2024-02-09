bcebc21128c716c227d7c443cfc991443e0951ed
kys-97 <115993353+kys-97@users.noreply.github.com>
2024-02-09 22:38:29 +0900

    Title: 장학금, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240209/장학금/scholarship.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a < 90) {
+            System.out.println(0);
+        }
+        else if (a >= 95) {
+            System.out.println(100000);
+        }
+        else if ((a>=90) && (a<95)) {
+            System.out.println(50000);
+        }
+    }
+}
