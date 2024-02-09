321d7557eb98c01e94b53d6b66b92e76faadece4
kys-97 <115993353+kys-97@users.noreply.github.com>
2024-02-09 22:58:41 +0900

    Title: 윤년인가, Time: 139ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240209/윤년인가/is-leap-year.java
+++ 240209/윤년인가/is-leap-year.java
@@ -4,8 +4,10 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int y = sc.nextInt();
-        if (y%4==0) {
-            if (y%100 == 0) {
+
+        if(y%4==0) {
+            System.out.println("true");
+            if(y%100 == 0) {
                 System.out.println("false");
                 if (y%400 == 0) {
                     System.out.println("true");
