87d81d9756c943ee815dd7c56e8927e0323923d3
kys-97 <115993353+kys-97@users.noreply.github.com>
2024-02-11 18:27:53 +0900

    Title: 369 게임, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240211/369 게임/369-game.java
+++ 240211/369 게임/369-game.java
@@ -4,10 +4,12 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int result = 0;
+        int result1 = 0;
+        int result2 =0;
 		for(int i = 1; i <= n; i++) {
-			result = i%10;
-			if (i%3 ==0 || result % 3 == 0) {
+			result1 = i%10;
+            result2 = i/10;
+			if (i%3 ==0 || result1 % 3 == 0 && result2 % 3 == 0) {
 				System.out.print(0+" ");
 			}
 			else {
