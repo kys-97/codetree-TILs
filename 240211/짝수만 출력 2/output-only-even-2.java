72434560e485273e239829fa5f03c892153fa2cf
kys-97 <115993353+kys-97@users.noreply.github.com>
2024-02-11 15:56:03 +0900

    Title: 짝수만 출력 2, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240211/짝수만 출력 2/output-only-even-2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int b = sc.nextInt();
+        int a = sc.nextInt();
+
+        while(b>=a) {
+            if (b%2 == 0) {
+                System.out.print(b+" ");
+            }
+            b--;
+        }
+    }
+}
