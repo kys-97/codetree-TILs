305222afff3f342ee8e7db7abea895cc109f7143
kys-97 <115993353+kys-97@users.noreply.github.com>
2024-02-14 08:21:09 +0900

    Title: 청소당번 정하기, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 240214/청소당번 정하기/cleaning-numbering.java
+++ 240214/청소당번 정하기/cleaning-numbering.java
@@ -1,28 +1,28 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
+        
+        // 변수 선언 및 입력
+        int n;
+        n = sc.nextInt();
 
-        int n = sc.nextInt();
-
-        int cnt1 = 0;
-        int cnt2 = 0;
-        int cnt3 = 0;
-
-        for(int i = 0; i < n; i++) {
-            //주기가 가장 긴 12일부터
-            if(i%12 == 0 && i!= 0) {
+        int cnt2 = 0, cnt3 = 0, cnt12 = 0;
+    
+        // 각 날짜마다 확인합니다.
+        for(int i = 1; i <= n; i++) {
+            // 주기가 가장 긴 12일부터 확인합니다.
+            if(i % 12 == 0)
+                cnt12++;
+            // 12일 주기에 들어오지 않는다면, 3일 주기에 들어오는지 확인합니다.
+            else if(i % 3 == 0)
                 cnt3++;
-            }
-            else if(i%3 == 0 && i!= 0) {
+            // 3일 주기에도 들어오지 않는다면, 2일 주기에 들어오는지 확인합니다.
+            else if(i % 2 == 0)
                 cnt2++;
-            }
-            else if(i%2 == 0 && i!= 0){
-                cnt1++;
-            }
         }
 
-        System.out.print(cnt1+" "+cnt2+" "+cnt3);
+        System.out.println(cnt2 + " " + cnt3 + " " + cnt12);
     }
 }
