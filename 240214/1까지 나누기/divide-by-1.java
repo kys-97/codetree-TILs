e4015169d7049599f9a2ce553a36d02c71ca805d
kys-97 <115993353+kys-97@users.noreply.github.com>
2024-02-14 14:33:19 +0900

    Title: 1까지 나누기, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 240214/1까지 나누기/divide-by-1.java
+++ 240214/1까지 나누기/divide-by-1.java
@@ -7,15 +7,20 @@ public class Main {
        //cnt
        int cnt = 0;
        //n 값 저장
-       double d = n;
+       int d = n;
        //int i = 1; i<=n; i++
+       //1부터 순서대로 나눔 -> d의 값이 1 이하가 되는 순간까지 나눗셈 진행 (break: if d<=1)
        for(int i = 1; i<=n; i++) {
-        d/=(double)i;
-        cnt++;
-        if(d<1) {
-            break;
-        }
+        d/=i;
+       cnt++;
+      // System.out.print(i);
+       //System.out.println(d);
+       if(d<=1) {
+        System.out.println(cnt);
+        break;
        }
-       System.out.print(cnt);
+       
+       }
+       
     }
 }
