0feb72b1b2a029067f45b62cc2b1aa96f83399e9
kys-97 <115993353+kys-97@users.noreply.github.com>
2024-02-14 09:42:39 +0900

    Title: 완전수 판별기, Time: 162ms, Memory: 10MB, Status: Passed - CodeTree

--- 240214/완전수 판별기/perfect-number-discriminator.java
+++ 240214/완전수 판별기/perfect-number-discriminator.java
@@ -3,20 +3,23 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
+        //n
         int n = sc.nextInt();
+        //sum
         int sum = 0;
 
-        for(int i = 1; i <= n; i++) {
-            if (i%n == 0) {
+        for(int i = 1; i<n; i++) {
+            if(n%i == 0) {
                 sum += i;
             }
         }
 
-        if (sum == n) {
+        if(sum == n) {
             System.out.println("P");
         }
         else {
             System.out.println("N");
         }
+       
     }
 }
