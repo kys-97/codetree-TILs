b2389b0c6099aa59c29a343e2fa3d1b18dc72d94
kys-97 <115993353+kys-97@users.noreply.github.com>
2024-02-14 10:36:44 +0900

    Title: 특정 조건의 온전수 구하기, Time: 160ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240214/특정 조건의 온전수 구하기/print-onjeonsu.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 1; i<=n; i++) {
+            if(i%2==0 || i%3==0 && i%9 != 0 || i%10==5) {
+                continue;
+            }
+            System.out.print(i+" ");
+        }
+
+        
+    }
+}
