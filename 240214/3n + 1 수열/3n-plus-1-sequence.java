435ad2768f86b57ed8d68d1e5eaccdb9ab3c4dce
kys-97 <115993353+kys-97@users.noreply.github.com>
2024-02-14 15:06:09 +0900

    Title: 3n + 1 수열, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240214/3n + 1 수열/3n-plus-1-sequence.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+        while(true) {
+            //if n == even -> /2
+            if(n%2==0) {
+                n/=2;
+                cnt++;
+            }
+            else if (n%2!=0) {
+                n=(n*3)+1;
+                cnt++;
+            }
+            if(n==1) {
+                System.out.println(cnt);
+                break;
+            }
+        }
+        
+    }
+}
