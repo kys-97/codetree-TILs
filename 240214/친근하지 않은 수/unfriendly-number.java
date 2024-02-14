bc23fefe8d1cc2c5b9246efac4f0530592ea02d4
kys-97 <115993353+kys-97@users.noreply.github.com>
2024-02-14 10:27:17 +0900

    Title: 친근하지 않은 수, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240214/친근하지 않은 수/unfriendly-number.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+
+        for(int i = 1; i<=n; i++) {
+            if(i%2==0 || i%3==0 || i%5==0) {
+                //System.out.println(i);
+                continue;
+                
+            }
+            cnt++;
+            //System.out.println(i);
+        }
+       
+        System.out.println(cnt);
+    }
+}
