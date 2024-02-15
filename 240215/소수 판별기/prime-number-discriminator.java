9daf540a32f169f5f563e7fa0a0a6f974cc01488
kys-97 <115993353+kys-97@users.noreply.github.com>
2024-02-15 09:45:50 +0900

    Title: 소수 판별기, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 240215/소수 판별기/prime-number-discriminator.java
+++ 240215/소수 판별기/prime-number-discriminator.java
@@ -7,7 +7,7 @@ public class Main {
         boolean flag = true;
 
         for(int i = 2; i< n; i++) {
-            if(i%n==0) {
+            if(n%i==0) {
                 flag = false;
             }
         }
