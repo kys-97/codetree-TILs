67548d0211d7738882669b62f6342a8d7621b826
kys-97 <115993353+kys-97@users.noreply.github.com>
2024-02-15 09:05:58 +0900

    Title: 합성수 판별기, Time: 208ms, Memory: 10MB, Status: Passed - CodeTree

--- 240215/합성수 판별기/factorization-discriminator.java
+++ 240215/합성수 판별기/factorization-discriminator.java
@@ -10,7 +10,7 @@ public class Main {
         for(int i = 1; i<=n; i++) {
             if(n%i == 0 && n > 1) {
                 cnt++;
-                if(cnt > 3) {
+                if(cnt > 2) {
                     flag = true;
                 }
             }
