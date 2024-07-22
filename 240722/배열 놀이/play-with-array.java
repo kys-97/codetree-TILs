0dd25039c7c687c1454a309962d0fb1ac34998e2
kys-97 <115993353+kys-97@users.noreply.github.com>
2024-07-22 17:01:53 +0900

    Title: 배열 놀이, Time: 198ms, Memory: 11MB, Status: Passed - CodeTree

--- 240722/배열 놀이/play-with-array.java
+++ 240722/배열 놀이/play-with-array.java
@@ -1,40 +1,45 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int length = sc.nextInt();
-        HashMap <Integer, Integer> hMap = new HashMap<>();
-        //array
-        int [] arr = new int [n];
 
+        // 배열 정의
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 
+        // 질의 처리
         for (int i = 0; i < length; i++) {
             int q = sc.nextInt();
             if (q == 1) {
+                // "1 a" 질의 처리
                 int a = sc.nextInt();
                 System.out.println(arr[a-1]);
             } else if (q == 2) {
+                // "2 b" 질의 처리
                 int b = sc.nextInt();
-                for (int j = 0; j < arr.length; j++) {
+                int index = -1;
+                for (int j = 0; j < n; j++) {
                     if (arr[j] == b) {
-                        System.out.println(j+1);
+                        index = j + 1;
+                        break; // 첫 번째로 발견된 인덱스에서 멈춤
                     }
                 }
-            } else {
-                int a = sc.nextInt();
-                int b = sc.nextInt();
-                for (int j = 0; j < arr.length; j++) {
-                    System.out.println(arr[a-1]+" "+arr[b-1]);
-                    break;
-                    }
-                }            
+                System.out.println(index == -1 ? 0 : index);
+            } else if (q == 3) {
+                // "3 s e" 질의 처리
+                int s = sc.nextInt();
+                int e = sc.nextInt();
+                for (int j = s - 1; j < e; j++) {
+                    System.out.print(arr[j] + " ");
+                }
+                System.out.println(); // 줄 바꿈
             }
+        }
 
-            sc.close();
+        sc.close();
     }
 }
