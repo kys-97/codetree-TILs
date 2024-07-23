26ad8aa22c29ede554509c73771aa4214f037ca7
kys-97 <115993353+kys-97@users.noreply.github.com>
2024-07-23 15:35:33 +0900

    Title: n개의 숫자 중 최소, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

--- 240723/n개의 숫자 중 최소/min-of-n-num.java
+++ 240723/n개의 숫자 중 최소/min-of-n-num.java
@@ -7,14 +7,23 @@ public class Main {
 
         int [] arr = new int [n];
 
-        int min = 100;
+        int min = 0;
+        int max = 0;
 
         int cnt = 0;
 
         for(int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
+            if (arr[i] > max) {
+                max = arr[i];
+                
+            }
+            min = max;
+        }
+
+        for(int i = 0; i < n; i++) {
             if (arr[i] < min) {
-                min = arr[i];
+            min = arr[i];
             }
         }
 
